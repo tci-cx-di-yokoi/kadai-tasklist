409baf34abbbdd2b408d62f4ff376079294b5e13
hiromasa.Yokoi <yokoi.hiromasa@trans-cosmos.co.jp>
2025-06-06 10:48:20 +0900

    add status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -37,10 +37,17 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
+        // トップページへリダイレクトさせる
         return redirect('/');
     }
 
@@ -73,7 +80,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -13,6 +13,11 @@
         </tr>
 
         <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+
+        <tr>
             <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
